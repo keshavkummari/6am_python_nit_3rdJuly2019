aa3646b4154c08fd6bda2fd8a5e12be1aa645d84
kchennakesavulu <chennakesavulu.kummari@outlook.com>
2019-07-09 07:00:25 +0530

    Basics-Day-II

---
+++ Basics/Overview_Part-I.py
@@ -0,0 +1,52 @@
+#Comments :
+
+#1. Single Line
+
+#1. #
+
+#2. ''
+
+#and
+
+#3. ""
+
+#2. Multi Line :
+
+#1. ''' '''
+
+#and
+
+#2. """ """
+
+
+# Rule of Creating Variables in Python
+
+
+# Single Line Comment  : #
+# Creating variables in python
+# A-Z     # Hash means single line comment
+# a-z
+# combinations of A-Z & a-z
+# 0-9  # Numbers
+# combinations of A-Z , a-z & 0-9
+# an underscore (_).
+# combinations of A-Z , a-z , 0-9 & _
+
+
+#!/usr/bin/pyhton  Shebang #! and Absolute path of the python software
+
+price = 10   # Number Data Types and Integer
+
+TAX = 2.5   # Number Data Types and Float
+
+ImaginaryPrice = 3 + 5j  # Number Data Types and Complex
+
+NAME01 = 'Guido'
+
+name02 = "Van"
+
+_0001 = '''Rossum'''
+
+# Built-In Functions
+
+print(price,type(price),id(price))
