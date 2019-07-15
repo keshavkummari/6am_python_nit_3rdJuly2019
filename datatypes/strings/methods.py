6c226e1f71f5637e21e4aea969fc28f8f9a67802
kchennakesavulu <chennakesavulu.kummari@outlook.com>
2019-07-15 07:05:52 +0530

    Methods

--- datatypes/strings/methods.py
+++ datatypes/strings/methods.py
@@ -1,5 +1,71 @@
-name = "guido van rossum"
+      # 01234567891011
+name = "Python Welcome to Python World Python"
+name1 = "GUIDO VAN ROSSUM"
+number_values = "44"
 
-print(name)
+str_ABC = "Python Variables Operators Functions Methods"
 
-print(name.capitalize())
+str_a = "Variables \nData \nTypes \nBuiltin \nFunctions List of Methods"
+
+#print(str_ABC.splitlines())
+#print(str_a.splitlines())
+
+print (str_ABC.splitlines( ))
+print(str_a)
+print (str_a.splitlines())
+
+"""
+name_1 = "ABC123 ABC123 ABC123 ABC123 ABC123 ABC123"
+
+print(name_1.split('C'))
+print(name.replace("KK","Keshav",2))
+
+print(name.startswith('Python'))
+print(name.endswith('Python'))
+
+
+alpha = "abcdefghijklmnopqrstuvwxyz"
+print(min(alpha))
+print(max(alpha))
+print(alpha.swapcase())
+alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(min(alpha_upper))
+print(max(alpha_upper))
+"""
+
+
+#print(name.title())
+#print(name)
+#print(name.strip('@'))
+#print(name.lstrip('@'))
+#print(name.rstrip('@'))
+             # 01234567
+#a_unitPrice = "Price49"
+#print(len(a_unitPrice))
+#print(a_unitPrice.rjust(10,'@'))
+#print(a_unitPrice.ljust(10,'$'))
+#s = 'Z'
+#print("Left to Right Search",name.find(s))
+#print("Right to Left Search",name.rfind(s))
+#print("Left to Right Index",name.index(s))
+#print("Right to Left Index",name.rindex(s))
+#print(name.count(s))
+#print("MacBook".count('o'))
+#number_values = U"44"
+#print(name.capitalize())
+#print(name.upper())
+#print(name1.lower())
+#print(name.swapcase())
+#print(name.isupper())
+#print(name1.isupper())
+#print(name.islower())
+#print(name1.islower())
+#print(number_values.isdigit())
+#print(number_values.isdecimal())
+#print(number_values.isnumeric())
+#c = "44"
+#print(c)
+#print(c.isdecimal())
+#print(c.isdigit())
+#print(c.isnumeric())
+#print(c.isdecimal(), c.isdigit(), c.isnumeric())
