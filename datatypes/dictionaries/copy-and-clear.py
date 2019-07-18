641d39f7158fc19b497997fad736f7bffb4b600e
kchennakesavulu <chennakesavulu.kummari@outlook.com>
2019-07-18 07:02:02 +0530

    numbers

---
+++ datatypes/dictionaries/copy-and-clear.py
@@ -0,0 +1,13 @@
+fileName = {'Course':'Python', 'type' : 'Programming', 'boolean': 1}
+
+sample = {1: "one", 2 : "two"}
+
+print(fileName)
+
+print(sample)
+
+raw_data = fileName.copy()
+
+print(raw_data)
+
+print(raw_data.clear())
---
+++ datatypes/numbers/overview_of_numbers.py
@@ -0,0 +1,38 @@
+
+# Integers : int()
+# Floating Point Numbers : float()
+# Complex Numbers : complex()
+
+"""
+a = 5 
+print(a, "is of type", type(a))
+
+a = 2.0
+print(a, 'is of type', type(a))
+
+a = 1-2J 
+print(a, "is complex numbers?", isinstance(1-2J, complex))
+
+"""
+import math
+"""
+a1 = 5
+print(math.fabs(a1))
+print(math.ceil(-10.1))
+print(math.floor(-10.7))
+print(math.modf(22.67))
+print(math.exp(6.1))
+print(math.sqrt(45))
+"""
+
+import random
+
+print("Random-Choice",random.choice([1,2,3,4,5,9]))
+
+print(random.randrange(0,10,2))
+
+print(random.random())
+random.seed(5)
+print(random.random())
+random.seed(5)
+print(random.random())
